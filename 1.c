90686a2f776277ed957844c160cb922847c836a0
Александра И. Цепкова <sasha.tsepkova@yandex.ru>
2018-03-22 07:37:38 +0100

    начала писать функцию для игры

--- 1.c
+++ 1.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-void menu(){
+#include <string.h>
+char* menu(){
 	int x; FILE*f;
 	int n=0, i;
 	char*a[12];
@@ -24,15 +25,41 @@ void menu(){
 		printf("error"); 
 		return; 
 	}
-	i=1+rand()%20;
-	while(i<=x){
+	i=1+rand()%20; 
+	while(i>=x){
 		fscanf(f,"%s",a);
+		x++;
 		}
 	return a;
 }
 
 
+void game(char *a) {  //передаем нашу строку со словом
+	int num, i; int try=7; char* used; char letter; //used - уже использованные буквы, letter - буква для считывания
+	num=strlen(a);
+	char* b[num]; 
+	for(i=0; i<num; i++){ 	//массив такой же длины как и наша строка заполняем звездочками, потом будем менять на буквы
+		b[i]=(char)42;
+	}
+	printf("%d букв\n", num);
+	printword(b, num); 	//отдельная функция для печати слова
+	printf("\nпопыток осталось: %d", try);
+	while(try>0){ 		//сам игровой процесс
+		printf("\nвведите букву");
+		
+	}
+}
+
+void printword(char* a, int num){ //печать слова
+	int i;
+	printf("\n");
+	for(i=0;i<num;i++){
+		printf("%c",a[i]);
+	}
+}
+
 int main(){
-	menu;
-	
+	char* word;
+	word=menu;
+	game(word);
 }
